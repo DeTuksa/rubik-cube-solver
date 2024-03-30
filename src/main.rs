56f93c83cebc1795028e337be8a5473e312c833c
DeTuksa <emmydave414@gmail.com>
2024-03-30 12:37:25 +0000

    read cube from user input and print cube

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
 use rubik_cube_solver::RubikCube;
 
 fn main() {
-    let cube = RubikCube::new();
-    println!("<<<<<<<<<<<<Front Face>>>>>>>>>>>");
-    cube.faces[0].print_face();
-    println!("<<<<<<<<<<<<Back Face>>>>>>>>>>>");
-    cube.faces[1].print_face();
-    println!("<<<<<<<<<<<<Up Face>>>>>>>>>>>");
-    cube.faces[2].print_face();
-    println!("<<<<<<<<<<<<Down Face>>>>>>>>>>>");
-    cube.faces[3].print_face();
-    println!("<<<<<<<<<<<<Left Face>>>>>>>>>>>");
-    cube.faces[4].print_face();
-    println!("<<<<<<<<<<<<Right Face>>>>>>>>>>>");
-    cube.faces[5].print_face();
+    let cube = RubikCube::read_from_input();
+    cube.print();
 }
--- src/rubik_cube.rs
+++ src/rubik_cube.rs
@@ -1,6 +1,8 @@
+use std::io::{self, Write};
+
 use super::face::{Face, Color};
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct  RubikCube {
     pub faces: [Face; 6]
 }
@@ -24,5 +26,99 @@ impl RubikCube {
                 orange_face
             ]
         }
+    }
+
+    pub fn select_face() -> usize {
+        loop {
+            println!("Select a face (0-5):");
+            println!("0. Front");
+            println!("1. Back");
+            println!("2. Up");
+            println!("3. Down");
+            println!("4. Left");
+            println!("5. Right");
+
+            print!("Enter face number: ");
+            io::stdout().flush().unwrap();
+            
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).expect("Failed to read line");
+
+            match input.trim().parse::<usize>() {
+                Ok(face_index @ 0..=5) => return face_index,
+                _ => println!("Invalid face number. Please enter a number between 0 and 5")
+            }
+        }    
+    }
+
+    pub fn read_from_input() -> Self {
+        let mut cube = RubikCube::new();
+
+        println!("Enter colors for each face:");
+        println!("(R)ed, (B)lue, (G)reen, (O)range, (W)hite, (Y)ellow");
+        println!("Enter the colors for each row of each face separated by spaces.");
+
+        for _ in 0..6 {
+            let face_idx = RubikCube::select_face();
+            let face_name = match face_idx {
+                0 => "Front",
+                1 => "Back",
+                2 => "Up",
+                3 => "Down",
+                4 => "Left",
+                5 => "Right",
+                _ => panic!("Invalid face index selected"),
+            };
+
+            println!("Enter colors for {} Face:", face_name);
+
+            for i in 0..3 {
+                print!("Row {}: ", i+1);
+                io::stdout().flush().unwrap();
+
+                let mut input = String::new();
+                io::stdin().read_line(&mut input).expect("Failed to read line");
+
+                let colors: Vec<&str> = input.trim().split_whitespace().collect();
+                if colors.len() != 3 {
+                    println!("Please enter 3 colors for each row");
+                    return  RubikCube::read_from_input();
+                }
+
+                for j in 0..3 {
+                    let color_char = colors[j].chars().next().unwrap();
+                    let color = match color_char {
+                        'R' => Color::Red,
+                        'B' => Color::Blue,
+                        'G' => Color::Green,
+                        'O' => Color::Orange,
+                        'W' => Color::White,
+                        'Y' => Color::Yellow,
+                        _ => {
+                            println!("Invalid color code.");
+                            return  RubikCube::read_from_input();
+                        }
+                    };
+                    cube.faces[face_idx].colors[i][j] = color;
+                }
+            }
+        }
+
+        cube
+    }
+
+    pub fn print(&self) {
+        println!("<<<<<<<<<<<<Front Face>>>>>>>>>>>");
+        self.faces[0].print_face();
+        println!("<<<<<<<<<<<<Back Face>>>>>>>>>>>");
+        self.faces[1].print_face();
+        println!("<<<<<<<<<<<<Up Face>>>>>>>>>>>");
+        self.faces[2].print_face();
+        println!("<<<<<<<<<<<<Down Face>>>>>>>>>>>");
+        self.faces[3].print_face();
+        println!("<<<<<<<<<<<<Left Face>>>>>>>>>>>");
+        self.faces[4].print_face();
+        println!("<<<<<<<<<<<<Right Face>>>>>>>>>>>");
+        self.faces[5].print_face();
     }
 }
